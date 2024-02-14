f2cfbe2b661b1559ca8ebceb76e1a8e491f0f5c0
adamsEugene <adamseugene292@gmail.com>
2024-02-14 02:04:36 +0000

    updated

--- components/TabsForCartItems.tsx
+++ components/TabsForCartItems.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import { Tabs, Tab } from "@nextui-org/react";
 import cartItems, { ItemsInCart } from "@/utils/cartItem";
 import CartItem from "./CartItem";
+import StyledImage from "./StyledImage";
+import ConditionalRenderAB from "./ConditionalRenderAB";
 
 const TabsForCartItems: React.FC<{}> = () => {
   const [allItems, setAllItems] = useState<ItemsInCart[]>(cartItems);
@@ -22,19 +24,55 @@ const TabsForCartItems: React.FC<{}> = () => {
         defaultSelectedKey={"default"}
       >
         <Tab key="default" title="Default (8)">
-          {allItems.map((item) => (
-            <CartItem key={item.key} item={item} removeItem={removeItem} />
-          ))}
+          <ConditionalRenderAB
+            ComponentA={
+              <>
+                {allItems.map((item) => (
+                  <CartItem
+                    key={item.key}
+                    item={item}
+                    removeItem={removeItem}
+                  />
+                ))}
+              </>
+            }
+            ComponentB={EmptyCart}
+            condition={allItems.length > 0}
+          />
         </Tab>
         <Tab key="leasing" title="Leasing (3)">
-          {allItems.map((item) => (
-            <CartItem key={item.key} item={item} removeItem={removeItem} />
-          ))}
+          <ConditionalRenderAB
+            ComponentA={
+              <>
+                {allItems.map((item) => (
+                  <CartItem
+                    key={item.key}
+                    item={item}
+                    removeItem={removeItem}
+                  />
+                ))}
+              </>
+            }
+            ComponentB={EmptyCart}
+            condition={allItems.length > 0}
+          />
         </Tab>
         <Tab key="rent" title="Rent  (4)">
-          {allItems.map((item) => (
-            <CartItem key={item.key} item={item} removeItem={removeItem} />
-          ))}
+          <ConditionalRenderAB
+            ComponentA={
+              <>
+                {allItems.map((item) => (
+                  <CartItem
+                    key={item.key}
+                    item={item}
+                    removeItem={removeItem}
+                  />
+                ))}
+              </>
+            }
+            ComponentB={EmptyCart}
+            condition={allItems.length > 0}
+          />
         </Tab>
       </Tabs>
     </div>
@@ -42,3 +80,19 @@ const TabsForCartItems: React.FC<{}> = () => {
 };
 
 export default TabsForCartItems;
+
+const EmptyCart = (
+  <div className="min-h-[500px] w-full flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-6">
+      <StyledImage
+        src="/assets/svgs/emptyCart.svg"
+        alt=" Your shopping cart is empty"
+        height={400}
+        width={300}
+      />
+      <p className="text-lg font-semibold text-gray-600">
+        Your shopping cart is empty
+      </p>
+    </div>
+  </div>
+);
