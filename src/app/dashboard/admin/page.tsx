50620f63b64a494e148d875f060015368e07c130
adamsEugene <adamseugene292@gmail.com>
2024-03-23 17:31:00 +0000

    started working on the admin dashboard

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next";
+import ProductMetrics from "../components/ProductMetrics";
+import { adminDashboardMetrics } from "@/src/utils/dashboardData";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -6,8 +8,22 @@ export const metadata: Metadata = {
 
 export default function AdminDashboard() {
   return (
-    <div className="home">
-      <div className="main">AdminDashboard</div>
+    <div className="flex flex-col gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
+        {adminDashboardMetrics.slice(0, 4).map((metrics, index) => (
+          <ProductMetrics {...metrics} key={index} />
+        ))}
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
+        {adminDashboardMetrics.slice(4, 8).map((metrics, index) => (
+          <ProductMetrics {...metrics} key={index} />
+        ))}
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
+        {adminDashboardMetrics.slice(8).map((metrics, index) => (
+          <ProductMetrics {...metrics} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
