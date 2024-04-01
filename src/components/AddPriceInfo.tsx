8f3167ebcace94226ea91631f476aa420924ba91
adamsEugene <adamseugene292@gmail.com>
2024-04-01 21:07:21 +0000

    almost done with product page

--- src/components/AddPriceInfo.tsx
+++ src/components/AddPriceInfo.tsx
@@ -1,11 +1,102 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
+import { IconType } from "react-icons";
 
 type PROPS = {
   type: "lease" | "rent" | "default";
+  data: BenefitProps[];
+  amount: string;
+  currency?: string;
+  duration: string;
 };
 
-export default function AddPriceInfo(props:PROPS) {
-    const {type} = props
+const AddPriceInfo: React.FC<PROPS> = ({
+  type,
+  data,
+  amount,
+  duration,
+  currency,
+}) => {
+  const [editing, setEditing] = useState(false);
 
-  return <div>okay</div>;
-}
+  const toggleEditing = () => {
+    setEditing((prevEditing) => !prevEditing);
+  };
+
+  return (
+    <div className="w-full h-full">
+      <div className="relative w-full h-full" onClick={toggleEditing}>
+        <AnimatePresence>
+          {!editing ? (
+            <motion.div
+              className="w-full h-full top-0 left-0 rounded-md"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FrontComponent
+                data={data}
+                amount={amount}
+                duration={duration}
+                currency={currency}
+              />
+            </motion.div>
+          ) : (
+            <motion.div
+              className="w-full h-full top-0 left-0 rounded-md"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <BackComponent />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+};
+
+type BenefitProps = {
+  icon: IconType;
+  benefit: string;
+};
+
+type Props = {
+  data: BenefitProps[];
+  amount: string;
+  currency?: string;
+  duration: string;
+};
+
+const FrontComponent = ({ data, amount, duration, currency }: Props) => {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <div className="flex items-baseline justify-center gap-1">
+        <span className="text-3xl font-bold">{currency}</span>
+        <p className="text-5xl font-bold">{amount}</p>
+        <span className="text-xl font-bold text-gray-500">/{duration}</span>
+      </div>
+      <ul className="list-disc text-base flex flex-col gap-3">
+        {data.map((item, index) => (
+          <li key={index} className="flex items-center gap-2">
+            {item.icon && (
+              <div className="h-5 w-5 rounded-lg border border-green-400 flex justify-center items-center">
+                <item.icon className="text-green-500" />
+              </div>
+            )}
+            <span>{item.benefit}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+const BackComponent = () => {
+  return <div>back</div>;
+};
+
+export default AddPriceInfo;
---
+++ src/components/CreateShippingOption.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  expressOption,
+  freeOption,
+  regularOption,
+} from "../utils/generateDataForSelect";
+
+type Option = {
+  icon?: React.ElementType;
+  benefit: string;
+};
+
+type ShippingProps = {
+  name: string;
+  price: string;
+  options: Option[];
+};
+
+const ShippingOption: React.FC<ShippingProps> = ({ name, price, options }) => (
+  <div className="flex-1 flex items-center flex-col gap-4">
+    <p className="text-3xl font-bold">{name}</p>
+    <div className="h-40 w-40 rounded-full flex justify-center items-center border-gray-400 border-[16px]">
+      <p className="text-2xl font-bold">{price}</p>
+    </div>
+    <ul className="list-disc text-base flex flex-col gap-3">
+      {options.map((item, index) => (
+        <li key={index} className="flex items-center gap-2">
+          {item.icon && (
+            <div
+              className={`h-5 w-5 rounded-lg border ${
+                item.benefit === "No tracking"
+                  ? "border-red-400"
+                  : "border-green-400"
+              } flex justify-center items-center`}
+            >
+              <item.icon
+                className={
+                  item.benefit === "No tracking"
+                    ? "text-red-500"
+                    : "text-green-500"
+                }
+              />
+            </div>
+          )}
+          <span>{item.benefit}</span>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+const CreateShippingOption: React.FC = () => {
+  return (
+    <div className="flex gap-3">
+      <ShippingOption name="Free" price="Free" options={freeOption} />
+      <ShippingOption name="Regular" price="GHS8" options={regularOption} />
+      <ShippingOption name="Express" price="GHS15" options={expressOption} />
+    </div>
+  );
+};
+
+export default CreateShippingOption;
