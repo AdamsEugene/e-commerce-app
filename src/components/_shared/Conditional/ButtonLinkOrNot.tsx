823bff171e154126f2a2c041315a54da662deb44
adamsEugene <adamseugene292@gmail.com>
2024-05-04 19:36:01 +0000

    fix build

--- src/components/_shared/Conditional/ButtonLinkOrNot.tsx
+++ src/components/_shared/Conditional/ButtonLinkOrNot.tsx
@@ -9,35 +9,39 @@ import { campaignMetrics } from "@/src/utils/dashboardData";
 export default function ButtonLinkOrNot() {
   const openModal = useAppStore((state) => state.openModal);
 
-  return campaignMetrics.map((item) => {
-    const Icon = item.icon;
+  return (
+    <>
+      {campaignMetrics.map((item) => {
+        const Icon = item.icon;
 
-    return (
-      <ConditionalRenderAB
-        key={item.value}
-        condition={!!item.link}
-        ComponentA={
-          <Button
+        return (
+          <ConditionalRenderAB
             key={item.value}
-            as={Link}
-            href={item.link}
-            style={{ background: item.bgColor }}
-            startContent={<Icon />}
-          >
-            {item.value}
-          </Button>
-        }
-        ComponentB={
-          <Button
-            key={item.value}
-            style={{ background: item.bgColor }}
-            startContent={<Icon />}
-            onClick={() => openModal("create_campaign")}
-          >
-            {item.value}
-          </Button>
-        }
-      />
-    );
-  });
+            condition={!!item.link}
+            ComponentA={
+              <Button
+                key={item.value}
+                as={Link}
+                href={item.link}
+                style={{ background: item.bgColor }}
+                startContent={<Icon />}
+              >
+                {item.value}
+              </Button>
+            }
+            ComponentB={
+              <Button
+                key={item.value}
+                style={{ background: item.bgColor }}
+                startContent={<Icon />}
+                onClick={() => openModal("create_campaign")}
+              >
+                {item.value}
+              </Button>
+            }
+          />
+        );
+      })}
+    </>
+  );
 }
